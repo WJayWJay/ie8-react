cef4a3630a5f8c5a2a4588ea47bd49386c647bed
hellokitty <wj342234130@gmail.com>
2018-10-19 08:23:39 +0800

    fix: config proxy

--- config/config.js
+++ config/config.js
@@ -3,15 +3,15 @@ module.exports = {
     host: '0.0.0.0',
     proxy: {
         // "/api": "https://jf.gf.com.cn/api",
-        "/api": {
-            target: "https://jf.gf.com.cn/",
+        "/private": {
+            target: "http://520.com:9001",
             secure: false,
-            pathRewrite: {"^/api" : ""}
+            // pathRewrite: {"^/prvate" : ""}
           },
         // {
         //     target: "https://jf.gf.com.cn/"
         // },
-        "/": "http://localhost:8001",
+        // "/": "http://localhost:8001",
         // "/public": {
         //     target: "http://localhost:8001",
         //     // pathRewrite: {"^/public" : ""}
